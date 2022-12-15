3c43ccf39688b9474296fa2e3828e8d5b95a7ac5
seongmin <csm5588@gmail.com>
2022-12-16 01:07:29 +0900

    회신용 오브젝트 생성

---
+++ TransJobAPI/Controllers/ExaminationHistoryMultipleChoiceDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransJobAPI.Controllers
+{
+    public class ExaminationHistoryMultipleChoiceDTO
+    {
+        public long ExaminationHistoryId { get; set; }
+        public long MultipleQuestionId { get; set; }
+        public long JobDefinitionId { get; set; }
+        public string Contents { get; set; }
+        public string Answer { get; set; }
+    }
+}
