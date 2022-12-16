29d3c4b7483ea38f2256ebef859aba19a65b6aa6
최성민 <csm5588@gmail.com>
2022-12-16 15:44:12 +0900

    db접속정보 변경

--- TransJobAPI/Contexts/InitializeTestDbContext.cs
+++ TransJobAPI/Contexts/InitializeTestDbContext.cs
@@ -33,7 +33,7 @@ namespace TransJobAPI.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=118.67.151.133,1433; Initial Catalog=JOBTRANS_KTL; User ID=ktl;Password=smart12@#34!;");
+                optionsBuilder.UseSqlServer("Data Source=KTL-DUTY-WEB-DB; Initial Catalog=JOBTRANS_KTL; User ID=ktl;Password=smart12@#34!;");
             }
         }
 
