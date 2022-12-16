e74b95c6c2ffa29303e08544ecbe4e21699c4e1c
최성민 <csm5588@gmail.com>
2022-12-16 14:45:43 +0900

    중복된 문제 발생시 처리

--- TransJobAPI/Controllers/SubmitAnswerAndGetNextQuestionController.cs
+++ TransJobAPI/Controllers/SubmitAnswerAndGetNextQuestionController.cs
@@ -16,21 +16,28 @@ namespace TransJobAPI.Controllers
     {
         const int MAX = 5;
         [HttpPost]
-        public ActionResult<ExaminationHistoryMultipleChoiceDTO> Post(ExaminationHistoryMultipleChoice question)
+        public ActionResult<ExaminationHistoryMultipleChoiceDTO> Post(ExaminationHistoryMultipleChoiceDTO question)
         {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest("answer is InValid");
+            }
             var db = new InitializeTestDbContext();
             var orders = db.JobDefinitionDepthThreeOrders.Where(p => p.ExaminationHistoryId == question.ExaminationHistoryId).ToList();
 
-            int examNum = (int)question.Seq;
+            var examinationHistoryMultipleChoices = db.ExaminationHistoryMultipleChoices.Where(
+                    p => p.ExaminationHistoryId == question.ExaminationHistoryId).OrderByDescending(p => p.Seq).ToList();
+
+
+            int examNum = (int)examinationHistoryMultipleChoices[0].Seq;
             int prevLevel;
             int nowLevel;
-
             {
                 /*채점*/
                 long questionId = question.MultipleQuestionId;
                 string userAnswer = question.Answer;
                 var t = db.ExaminationHistoryMultipleChoices.Where(p => p.ExaminationHistoryId == question.ExaminationHistoryId && p.MultipleQuestionId == question.MultipleQuestionId).FirstOrDefault();
-         
+
 
                 if (String.Equals(db.MultipleChoiceQuestions.Find(questionId).RealAnswer, userAnswer) == true)
                 {
@@ -45,14 +52,11 @@ namespace TransJobAPI.Controllers
 
                 if (examNum % 5 != 1)
                 {
-                    var examinationHistoryMultipleChoices = db.ExaminationHistoryMultipleChoices.Where(
-                    p => p.ExaminationHistoryId == question.ExaminationHistoryId).OrderByDescending(p => p.Seq).ToList();
-
                     int nowMultipleQuestionId = (int)examinationHistoryMultipleChoices[0].MultipleQuestionId;
                     bool bNowIsRight = (bool)examinationHistoryMultipleChoices[0].Whether;
                     var nowQuestion = db.MultipleChoiceQuestions.Where(p => p.Id == nowMultipleQuestionId).FirstOrDefault();
                     nowLevel = (int)nowQuestion.QuestionLevel;
-                    
+
                     int prevMultipleQuestionId = (int)examinationHistoryMultipleChoices[1].MultipleQuestionId;
                     bool bPrevIsRight = (bool)examinationHistoryMultipleChoices[1].Whether;
                     var prevQuestion = db.MultipleChoiceQuestions.Where(p => p.Id == prevMultipleQuestionId).FirstOrDefault();
@@ -115,22 +119,31 @@ namespace TransJobAPI.Controllers
                         questionPool.Add(questions[i]);
                     }
                 }
-
+        EXTRACT_RANDOM_IDX:
                 Random rnd = new Random();
                 int randomIdx = rnd.Next(questionPool.Count());
 
-                ExaminationHistoryMultipleChoice emc = new ExaminationHistoryMultipleChoice();
-                emc.ExaminationHistoryId = question.ExaminationHistoryId;
-                emc.MultipleQuestionId = questionPool[randomIdx].Id;
-                emc.Seq = question.Seq + 1;
-                emc.Answer = null;
-                emc.Whether = null;
-                emc.EmployeeId = question.EmployeeId;
+                ExaminationHistoryMultipleChoice emc = new ExaminationHistoryMultipleChoice
+                {
+                    EmployeeId = question.EmployeeId,
+                    ExaminationHistoryId = question.ExaminationHistoryId,
+                    MultipleQuestionId = questionPool[randomIdx].Id,
+                    Seq = examNum + 1,
+                    Answer = null,
+                    Whether = null
+                };
+                bool bIsDuplicated = db.ExaminationHistoryMultipleChoices.Where(p => p.ExaminationHistoryId == emc.ExaminationHistoryId &&
+                p.MultipleQuestionId == emc.MultipleQuestionId).Count() > 0;
 
+                
+
+                if (bIsDuplicated == true)
+                {
+                    goto EXTRACT_RANDOM_IDX;
+                }
                 db.ExaminationHistoryMultipleChoices.Add(emc);
                 db.SaveChanges();
 
-                var e = db.QuestionJobDefinitions.Where(p => p.QuestionId == questionPool[randomIdx].QuestionId).FirstOrDefault();
 
                 StringBuilder sb = new StringBuilder(512);
                 sb.Append("#");
@@ -141,13 +154,15 @@ namespace TransJobAPI.Controllers
                 sb.Append(questionPool[randomIdx].Answer3);
                 sb.Append("#");
                 sb.Append(questionPool[randomIdx].Answer4);
+                var qJobDefinition = db.QuestionJobDefinitions.Where(p => p.QuestionId == questionPool[randomIdx].QuestionId).FirstOrDefault();
 
                 ExaminationHistoryMultipleChoiceDTO emcDTO = new ExaminationHistoryMultipleChoiceDTO()
                 {
+
+                    EmployeeId = emc.EmployeeId,
                     ExaminationHistoryId = emc.ExaminationHistoryId,
                     MultipleQuestionId = emc.MultipleQuestionId,
-                    JobDefinitionId = e.JobDefinitionId,
-
+                    JobDefinitionId = qJobDefinition.JobDefinitionId,
                     Answer = sb.ToString(),
                     Contents = questionPool[randomIdx].QuestionContents
                 };
